468d00e00bc27a07eb9012f0dcc2882f66aada6c
yxdtg <2430877819@qq.com>
2025-08-06 16:29:56 +0800

    初始化

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { Engine } from "./engine";
+
+export const engine = new Engine();
---
+++ tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+    entry: "./src/index.ts",
+    dts: true,
+    outExtensions: (context) => {
+        return {
+            js: ".js",
+        };
+    },
+    clean: true,
+});
