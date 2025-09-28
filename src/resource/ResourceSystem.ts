aa97ab208444d2ff693dcd83fe4e73524ee38a55
yxdtg <2430877819@qq.com>
2025-09-29 01:21:54 +0800

    优化资源系统..

--- src/resource/ResourceSystem.ts
+++ src/resource/ResourceSystem.ts
@@ -1,14 +1,21 @@
 import {
+    AtlasAttachmentLoader,
+    SkeletonBinary,
+    SkeletonJson,
+    SpineTexture,
+    type SkeletonData,
+    type SpineOptions,
+    type TextureAtlas,
+} from "@esotericsoftware/spine-pixi-v8";
+import { resourceSystem } from "cassia-engine";
+import { Howl } from "howler";
+import { Assets, Texture } from "pixi.js";
+import {
+    RESOURCE_TYPE,
     type ILoadResourceInfo,
+    type ILoadResourceInfoOptions,
     type ILoadTargetTypeResourceInfo,
     type IUnloadResourceInfo,
-    loadAudio,
-    loadSpineSkeleton,
-    loadTexture,
-    RESOURCE_TYPE,
-    unloadAudio,
-    unloadSpineSkeleton,
-    unloadTexture,
 } from "./define";
 import { AudioResource, Resource, SpineSkeletonResource, TextureResource } from "./resources";
 
@@ -42,12 +49,12 @@ export class ResourceSystem {
                 },
                 [RESOURCE_TYPE.SpineSkeleton]: async (): Promise<SpineSkeletonResource> => {
                     try {
-                        const atlasUrl = options?.atlasUrl;
+                        const atlasSrc = options?.atlasSrc;
                         const textureNames = options?.textureNames;
-                        if (!atlasUrl || !textureNames || textureNames.length === 0)
+                        if (!atlasSrc || !textureNames || textureNames.length === 0)
                             throw new Error("Invalid options for SpineSkeletonResource");
 
-                        const spineSkeleton = await loadSpineSkeleton(src, atlasUrl, textureNames);
+                        const spineSkeleton = await loadSpineSkeleton(src, atlasSrc, textureNames);
                         const resource = new SpineSkeletonResource(loadResourceInfo, spineSkeleton);
                         return resource;
                     } catch (e) {
@@ -87,13 +94,21 @@ export class ResourceSystem {
 
     public async loadTypeResources(
         type: RESOURCE_TYPE,
-        loadTargetTypeResourceInfos: ILoadTargetTypeResourceInfo[]
+        loadTargetTypeResourceInfos: ILoadTargetTypeResourceInfo[],
+        options: ILoadResourceInfoOptions = {}
     ): Promise<void> {
         try {
             const loadResourceInfos = loadTargetTypeResourceInfos.map((loadTargetTypeResourceInfo) => {
+                const { options: loadTargetTypeResourceInfoOptions, ...loadTargetTypeResourceInfoArgs } =
+                    loadTargetTypeResourceInfo;
+
                 const loadResourceInfo: ILoadResourceInfo = {
                     type: type,
-                    ...loadTargetTypeResourceInfo,
+                    options: {
+                        ...options,
+                        ...loadTargetTypeResourceInfoOptions,
+                    },
+                    ...loadTargetTypeResourceInfoArgs,
                 };
                 return loadResourceInfo;
             });
@@ -104,25 +119,34 @@ export class ResourceSystem {
         }
     }
 
-    public async loadTextures(loadTextureResourceInfos: ILoadTargetTypeResourceInfo[]): Promise<void> {
+    public async loadTextures(
+        loadTextureResourceInfos: ILoadTargetTypeResourceInfo[],
+        options: ILoadResourceInfoOptions = {}
+    ): Promise<void> {
         try {
-            await this.loadTypeResources(RESOURCE_TYPE.Texture, loadTextureResourceInfos);
+            await this.loadTypeResources(RESOURCE_TYPE.Texture, loadTextureResourceInfos, options);
         } catch (e) {
             throw e;
         }
     }
 
-    public async loadAudios(loadAudioResourceInfos: ILoadTargetTypeResourceInfo[]): Promise<void> {
+    public async loadAudios(
+        loadAudioResourceInfos: ILoadTargetTypeResourceInfo[],
+        options: ILoadResourceInfoOptions = {}
+    ): Promise<void> {
         try {
-            await this.loadTypeResources(RESOURCE_TYPE.Audio, loadAudioResourceInfos);
+            await this.loadTypeResources(RESOURCE_TYPE.Audio, loadAudioResourceInfos, options);
         } catch (e) {
             throw e;
         }
     }
 
-    public async loadSpineSkeletons(loadSpineSkeletonResourceInfos: ILoadTargetTypeResourceInfo[]): Promise<void> {
+    public async loadSpineSkeletons(
+        loadSpineSkeletonResourceInfos: ILoadTargetTypeResourceInfo[],
+        options: ILoadResourceInfoOptions = {}
+    ): Promise<void> {
         try {
-            await this.loadTypeResources(RESOURCE_TYPE.SpineSkeleton, loadSpineSkeletonResourceInfos);
+            await this.loadTypeResources(RESOURCE_TYPE.SpineSkeleton, loadSpineSkeletonResourceInfos, options);
         } catch (e) {
             throw e;
         }
@@ -155,7 +179,7 @@ export class ResourceSystem {
                 },
                 [RESOURCE_TYPE.SpineSkeleton]: async (): Promise<void> => {
                     try {
-                        await unloadSpineSkeleton(resource.src, resource.options!.atlasUrl!);
+                        await unloadSpineSkeleton(resource.src, resource.options!.atlasSrc!);
                     } catch (e) {
                         throw e;
                     }
@@ -264,4 +288,104 @@ interface IResourceTypeMap {
     [RESOURCE_TYPE.Texture]: TextureResource;
     [RESOURCE_TYPE.Audio]: AudioResource;
     [RESOURCE_TYPE.SpineSkeleton]: SpineSkeletonResource;
+}
+
+async function loadTexture(url: string): Promise<Texture> {
+    try {
+        const texture = await Assets.load(url);
+        return texture;
+    } catch (e) {
+        throw e;
+    }
+}
+
+async function unloadTexture(url: string): Promise<void> {
+    try {
+        await Assets.unload(url);
+    } catch (e) {
+        throw e;
+    }
+}
+
+function loadAudio(url: string): Promise<Howl> {
+    return new Promise((resolve, reject) => {
+        try {
+            const audio = new Howl({
+                src: [url],
+            });
+            audio.on("load", () => {
+                resolve(audio);
+            });
+            audio.on("loaderror", (id, error) => {
+                reject(error);
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
+async function unloadAudio(audio: Howl): Promise<void> {
+    try {
+        audio.unload();
+    } catch (e) {
+        throw e;
+    }
+}
+
+async function loadSpineSkeleton(skelUrl: string, atlasUrl: string, textureNames: string[]): Promise<SkeletonData> {
+    try {
+        const skelData = await Assets.load(skelUrl);
+        const atlasData = await Assets.load(atlasUrl);
+
+        const textures: Texture[] = [];
+
+        textureNames.forEach((textureName) => {
+            const textureResource = resourceSystem.getTexture(textureName);
+            if (textureResource) {
+                textures.push(textureResource.data);
+            }
+        });
+
+        const skeletonData = parseSkeletonData(skelData, atlasData, textures);
+        return skeletonData;
+    } catch (e) {
+        throw e;
+    }
+}
+
+async function unloadSpineSkeleton(skelUrl: string, atlasUrl: string): Promise<void> {
+    try {
+        await Assets.unload(skelUrl);
+        await Assets.unload(atlasUrl);
+    } catch (e) {
+        throw e;
+    }
+}
+
+function parseSkeletonData(
+    skelData: Uint8Array,
+    atlasData: TextureAtlas,
+    textures: Texture[],
+    options?: SpineOptions
+): SkeletonData {
+    let skeleton: SkeletonData = null!;
+    atlasData.pages.forEach((page, index) => {
+        const texture = textures[index];
+        if (texture) {
+            const spineTexture = SpineTexture.from(texture.source);
+            page.setTexture(spineTexture);
+        } else {
+            console.warn(`Texture for page ${page.name} not found in provided images.`);
+        }
+    });
+
+    const attachmentLoader = new AtlasAttachmentLoader(atlasData);
+    let parser =
+        skelData instanceof Uint8Array ? new SkeletonBinary(attachmentLoader) : new SkeletonJson(attachmentLoader);
+
+    parser.scale = (options?.scale as number) ?? 1;
+    skeleton = parser.readSkeletonData(skelData);
+
+    return skeleton;
 }
--- src/resource/define.ts
+++ src/resource/define.ts
@@ -1,16 +1,3 @@
-import {
-    AtlasAttachmentLoader,
-    SkeletonBinary,
-    SkeletonJson,
-    SpineTexture,
-    type SkeletonData,
-    type SpineOptions,
-    type TextureAtlas,
-} from "@esotericsoftware/spine-pixi-v8";
-import { Howl } from "howler";
-import { Assets, Texture } from "pixi.js";
-import { resourceSystem } from "cassia-engine";
-
 export const RESOURCE_TYPE = {
     Texture: "texture",
     Audio: "audio",
@@ -22,21 +9,23 @@ export interface ILoadResourceInfo {
     name: string;
     type: RESOURCE_TYPE;
     src: string;
-    options?: {
-        /**
-         * type: "texture"
-         */
-        pixelStyle?: boolean;
+    options?: ILoadResourceInfoOptions;
+}
 
-        /**
-         * type: "spine-skeleton"
-         */
-        atlasUrl?: string;
-        /**
-         * type: "spine-skeleton"
-         */
-        textureNames?: string[];
-    };
+export interface ILoadResourceInfoOptions {
+    /**
+     * @type RESOURCE_TYPE.Texture
+     */
+    pixelStyle?: boolean;
+
+    /**
+     * @type RESOURCE_TYPE.SpineSkeleton
+     */
+    atlasSrc?: string;
+    /**
+     * @type RESOURCE_TYPE.SpineSkeleton
+     */
+    textureNames?: string[];
 }
 
 export type ILoadTargetTypeResourceInfo = Omit<ILoadResourceInfo, "type">;
@@ -44,107 +33,4 @@ export type ILoadTargetTypeResourceInfo = Omit<ILoadResourceInfo, "type">;
 export interface IUnloadResourceInfo {
     type: RESOURCE_TYPE;
     name: string;
-}
-
-export async function loadTexture(url: string): Promise<Texture> {
-    try {
-        const texture = await Assets.load(url);
-        return texture;
-    } catch (e) {
-        throw e;
-    }
-}
-export async function unloadTexture(url: string): Promise<void> {
-    try {
-        await Assets.unload(url);
-    } catch (e) {
-        throw e;
-    }
-}
-
-export function loadAudio(url: string): Promise<Howl> {
-    return new Promise((resolve, reject) => {
-        try {
-            const audio = new Howl({
-                src: [url],
-            });
-            audio.on("load", () => {
-                resolve(audio);
-            });
-            audio.on("loaderror", (id, error) => {
-                reject(error);
-            });
-        } catch (e) {
-            reject(e);
-        }
-    });
-}
-
-export async function unloadAudio(audio: Howl): Promise<void> {
-    try {
-        audio.unload();
-    } catch (e) {
-        throw e;
-    }
-}
-
-export async function loadSpineSkeleton(
-    skelUrl: string,
-    atlasUrl: string,
-    textureNames: string[]
-): Promise<SkeletonData> {
-    try {
-        const skelData = await Assets.load(skelUrl);
-        const atlasData = await Assets.load(atlasUrl);
-
-        const textures: Texture[] = [];
-
-        textureNames.forEach((textureName) => {
-            const textureResource = resourceSystem.getTexture(textureName);
-            if (textureResource) {
-                textures.push(textureResource.data);
-            }
-        });
-
-        const skeletonData = parseSkeletonData(skelData, atlasData, textures);
-        return skeletonData;
-    } catch (e) {
-        throw e;
-    }
-}
-
-export async function unloadSpineSkeleton(skelUrl: string, atlasUrl: string): Promise<void> {
-    try {
-        await Assets.unload(skelUrl);
-        await Assets.unload(atlasUrl);
-    } catch (e) {
-        throw e;
-    }
-}
-
-function parseSkeletonData(
-    skelData: Uint8Array,
-    atlasData: TextureAtlas,
-    textures: Texture[],
-    options?: SpineOptions
-): SkeletonData {
-    let skeleton: SkeletonData = null!;
-    atlasData.pages.forEach((page, index) => {
-        const texture = textures[index];
-        if (texture) {
-            const spineTexture = SpineTexture.from(texture.source);
-            page.setTexture(spineTexture);
-        } else {
-            console.warn(`Texture for page ${page.name} not found in provided images.`);
-        }
-    });
-
-    const attachmentLoader = new AtlasAttachmentLoader(atlasData);
-    let parser =
-        skelData instanceof Uint8Array ? new SkeletonBinary(attachmentLoader) : new SkeletonJson(attachmentLoader);
-
-    parser.scale = (options?.scale as number) ?? 1;
-    skeleton = parser.readSkeletonData(skelData);
-
-    return skeleton;
 }
